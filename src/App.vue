0668180931ff58e31b500d9043b12d9735200711
longy <longy@sucsoft.com>
2022-06-29 18:08:48 +0800

    feat:引入mockjs

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,8 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-})
+<script lang="ts" setup>
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 <style>
 #app {
